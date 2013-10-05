a34cff9c6c5afb340120f4c5cdec7bf9dfb25458
Joseph Lindlbauer <lindlba2@msu.edu>
2013-10-05 16:05:23 -0400

    Big Changes
    - Tin Can method calls are all made by "background.js"
    - Data concerning state is stored within "background.js" and can be
    managed calling the login, logout, and/or sendStatement methods.
    - In order to get selected text from a tab, a contentscript was created
    to pass messages to and from the webpage and extension.

---
+++ background.js
@@ -0,0 +1,118 @@
+var tincan;
+var loggedIn = 0;
+
+// BEGIN CONTEXT MENU
+
+var id = chrome.contextMenus.create(
+	{
+		"title": "Send Selection", 
+		"contexts": ["selection"],
+		"onclick": sendSelection
+		});
+
+function sendSelection () {
+	if(loggedIn == 0)
+		login(localStorage.LRSUsername, localStorage.LRSPassword, sendStatement);
+	else
+		sendStatement();
+}
+
+// END CONTEXT MENU
+
+// BEGIN TIN CAN
+
+login = function (m_username, m_password, success) {
+	console.log("Logging in...");
+
+	tincan = new TinCan ({
+  		recordStores: [{
+          endpoint: "http://35.9.22.105:8000/xapi/",
+          username: m_username,
+          password: m_password
+        }]
+	});
+
+	tincan.getStatements(
+	{
+	  // 'params' is passed through to TinCan.LRS.queryStatements
+	  params: {
+	    since: "2013-09-22 07:42:10CDT",
+	    limit: 1
+	  },
+	  callback: function (err, result) {
+	    if (err !== null) {
+	      loggedIn = 0;
+	    }
+	    else {
+	      loggedIn = 1;
+	      success();
+	    }
+		localStorage.LRSLoggedIn = loggedIn;
+		localStorage.LRSUsername = m_username;
+		localStorage.LRSPassword = m_password;
+      }
+	});
+}
+
+sendStatement = function (button) {
+  console.log("begin sending...");
+  if(button) button.attr("disabled", "disabled");
+  console.log("getting tab...");
+  getCurrentTab (function (tab) {
+      if(tab) {
+        console.log("getting selected text...");
+        // Get selected text
+        chrome.tabs.sendMessage(tab.id, {method: "getSelection"}, function (response) {
+          console.log("creating statement...");
+          var statement = {
+            actor : {
+              "objectType" : "Agent",
+              "mbox" : "mailto:" + localStorage.LRSUsername
+            },
+            verb : {
+              "id" : "http://adlnet.gov/expapi/verbs/experienced",
+              "display" : {}
+            },
+            object : {
+              "id" : tab.url,
+              "definition" : {
+                "name": {"en-US":tab.title},
+                "description": {"en-US": response.data}
+              }
+            }
+          };
+          console.log("statement created...");
+
+          console.log("sending statement...");
+          tincan.sendStatement(statement, function () {
+            console.log("Statement Sent.");
+            if(button) button.html ("Sent!");
+          });
+        });
+      } else {
+        console.log("Failed to send Statement");
+        if(button) button.attr("disabled", false);
+      }
+  });
+}
+
+logout = function (callback) {
+  console.log("Logging out.");
+  if(loggedIn != 0) {
+    loggedIn = 0;
+    callback();
+  }
+  localStorage.LRSLoggedIn = 0;
+}
+
+// END TIN CAN
+
+function getCurrentTab(callback) {
+  chrome.tabs.query ({
+      active: true,
+      currentWindow: true
+    }, function (tabs) {
+      callback(tabs[0]);
+    });
+}
+
--- popup.js
+++ popup.js
@@ -1,20 +1,18 @@
-var tincan;
-var loggedIn = 0;
-
+var bgScript = chrome.extension.getBackgroundPage();
 var transitionAnimationLength = 200;  // in ms
 
 $(document).ready ( function () {
 
 	// EVENTS
 
-  	$("#sendStatementButton").click (function () {sendStatement();});
+  	$("#sendStatementButton").click (function () {bgScript.sendStatement($("#sendStatementButton"))});
 
   	$("#loginButton").click (function () {
-      login($("#username").val(), $("#password").val());
+      bgScript.login($("#username").val(), $("#password").val(), loginAnimation);
   	});
 
     $("#logoutButton").click (function () {
-      logout();
+      bgScript.logout(logoutAnimation);
     });
 
     $("#gotoWebsite").click (function () {
@@ -27,122 +25,35 @@ $(document).ready ( function () {
 
   // END EVENTS
 
-  loggedIn = 0;
-  if (localStorage.LRSLoggedIn) {
-    loggedIn = localStorage.LRSLoggedIn;
-  };
-  console.log("Logged in? " + loggedIn);
+  // LOGIN SEQUENCE
 
-  if (loggedIn == 1) {
-      var _username = localStorage.LRSUsername;
-      var _password = localStorage.LRSPassword;
-      login (_username, _password);
-      return;
-  }
-  $("#userPanel").hide();
-  $("#panel").slideDown (transitionAnimationLength);
- });
-
-function login(m_username, m_password) {
-  console.log("Logging in...");
-  loggedIn = 0;
-  tincan = new TinCan ({
-      recordStores: [{
-          endpoint: "http://35.9.22.105:8000/xapi/",
-          username: m_username,
-          password: m_password
-        }]
-    });
-
-  //tincan.recordStores[0].auth = "Basic " + TinCan.Utils.getBase64String(m_username + ":" + m_password);
-
-  tincan.getStatements(
-    {
-      // 'params' is passed through to TinCan.LRS.queryStatements
-      params: {
-        since: "2013-09-22 07:42:10CDT",
-        limit: 1
-      },
-      callback: function (err, result) {
-        // 'err' will be null on success
-        if (err !== null) {
-          // handle error
-          loggedIn = 0;
-          return;
-        }
-        else {
-          loggedIn = 1;
-          $("#sendStatementButton").attr("disabled", false);
-          $("#sendStatementButton").html ("Send");
-          $("#panel").slideUp (transitionAnimationLength, function () {
-            $("#userPanel").show();
-            $("#loginPanel").hide();
-            $("#panel").slideDown (transitionAnimationLength);
-          });
-        }
-        localStorage.LRSLoggedIn = loggedIn;
-        localStorage.LRSUsername = m_username;
-        localStorage.LRSPassword = m_password;
-      }
-    }
-  );
-}
-
-function logout () {
-  console.log("Logging out.");
-  if(loggedIn == 1) {
-    loggedIn = 0;
-    $("#panel").slideUp (transitionAnimationLength, function () {
-      $("#userPanel").hide();
-      $("#loginPanel").show();
-      $("#panel").slideDown (transitionAnimationLength);
-    });
-  }
-  localStorage.LRSLoggedIn = 0;
-}
-
-function sendStatement () {
-  $("#sendStatementButton").attr("disabled", "disabled");
-  getCurrentTab (function (tab) {
-      if(tab) {
-        var statement = {
-          actor : {
-            "objectType" : "Agent",
-            "mbox" : "mailto:"
-          },
-          verb : {
-            "id" : "",
-            "display" : {}
-          },
-          object : {
-            "id" : "",
-            "definition" : {
-              "name": {"en-US":tab.title}
-            }
-          }
-      };
-
-      statement.actor.mbox += localStorage.LRSUsername;
-      statement.verb.id = "http://adlnet.gov/expapi/verbs/experienced";
-      statement.object.id = tab.url;
-      
-      tincan.sendStatement(statement, function () {
-        console.log("Statement Sent");
-        $("#sendStatementButton").html ("Sent!");
-      });
+  if(bgScript.loggedIn != 0) {
+    loginAnimation();
+  } else {
+    if(localStorage.LRSLoggedIn == 1) {
+      bgScript.login(localStorage.LRSUsername, localStorage.LRSPassword, loginAnimation);
     } else {
-      console.log("Failed to send Statement");
-      $("#sendStatementButton").attr("disabled", "false");
+      logoutAnimation();
     }
+  }
+
+  // END LOGIN SEQUENCE
+});
+
+function loginAnimation() {
+  $("#sendStatementButton").attr("disabled", false);
+  $("#sendStatementButton").html ("Send");
+  $("#panel").slideUp (transitionAnimationLength, function () {
+    $("#userPanel").show();
+    $("#loginPanel").hide();
+    $("#panel").slideDown (transitionAnimationLength);
   });
 }
 
-function getCurrentTab(callback) {
-  chrome.tabs.query ({
-      active: true, 
-      highlighted: true, 
-      currentWindow: true
-    }, function (tabs) {
-      callback(tabs[0]);
-    });
+function logoutAnimation() {
+  $("#panel").slideUp (transitionAnimationLength, function () {
+    $("#userPanel").hide();
+    $("#loginPanel").show();
+    $("#panel").slideDown (transitionAnimationLength);
+  });
 }
